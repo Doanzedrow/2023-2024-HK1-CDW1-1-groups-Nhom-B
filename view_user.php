d66e1ca5246a56e4b4274455e9754ff76bfd68ae
Doanzedrow <Tranhuudoan2003@gmail.com>
2023-10-18 01:18:29 +0700

    update hmac

--- view_user.php
+++ view_user.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once 'models/UserModel.php';
 $userModel = new UserModel();
 
@@ -7,7 +8,11 @@ $id = NULL;
 
 if (!empty($_GET['id'])) {
     $id = $_GET['id'];
-    $user = $userModel->findUserById($id);//Update existing user
+    //$resId = base64_decode($id); // Giải mã hóa id
+    $hmac = $_SESSION['hmac'];
+    $key = 'khonghack'; // Khóa bí mật
+    $server_hmac = hash_hmac('sha256', $id, $key); // Tạo lại mã băm HMAC tại máy chủ
+    $user = $userModel->findUserByMD5Id($id); //Update existing user
 }
 
 
@@ -24,38 +29,54 @@ if (!empty($_POST['submit'])) {
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>User form</title>
     <?php include 'views/meta.php' ?>
 </head>
-<body>
-<?php include 'views/header.php'?>
-<div class="container">
 
-    <?php if ($user || empty($id)) { ?>
-        <div class="alert alert-warning" role="alert">
-            User profile
-        </div>
-        <form method="POST">
-            <input type="hidden" name="id" value="<?php echo $id ?>">
-            <div class="form-group">
-                <label for="name">Name</label>
-                <span><?php if (!empty($user[0]['name'])) echo $user[0]['name'] ?></span>
-            </div>
-            <div class="form-group">
-                <label for="password">Fullname</label>
-                <span><?php if (!empty($user[0]['name'])) echo $user[0]['fullname'] ?></span>
-            </div>
-            <div class="form-group">
-                <label for="password">Email</label>
-                <span><?php if (!empty($user[0]['name'])) echo $user[0]['email'] ?></span>
-            </div>
-        </form>
-    <?php } else { ?>
-        <div class="alert alert-success" role="alert">
-            User not found!
-        </div>
-    <?php } ?>
-</div>
+<body>
+    <?php include 'views/header.php' ?>
+    <div class="container">
+        <?php if ($hmac === $server_hmac) { ?>
+            <?php if ($user || empty($id)) { ?>
+                <div class="alert alert-warning" role="alert">
+                    User profile
+                </div>
+                <form method="POST">
+                    <input type="hidden" name="id" value="<?php echo $id ?>">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <span>
+                            <?php if (!empty($user[0]['name']))
+                                echo $user[0]['name'] ?>
+                            </span>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Fullname</label>
+                            <span>
+                            <?php if (!empty($user[0]['name']))
+                                echo $user[0]['fullname'] ?>
+                            </span>
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Email</label>
+                            <span>
+                            <?php if (!empty($user[0]['name']))
+                                echo $user[0]['email'] ?>
+                            </span>
+                        </div>
+                    </form>
+            <?php } else { ?>
+                <div class="alert alert-success" role="alert">
+                    User not found!
+                </div>
+            <?php } ?>
+        <?php } else {
+            header('location: error.php');
+        }
+        ?>
+    </div>
 </body>
+
 </html>
