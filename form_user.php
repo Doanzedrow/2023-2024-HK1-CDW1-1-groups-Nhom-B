b38ab626f7d5c168f803ec5cbd0c608cc4059e38
Doanzedrow <Tranhuudoan2003@gmail.com>
2023-10-19 03:34:51 +0700

    Update CSRF

--- form_user.php
+++ form_user.php
@@ -12,24 +12,41 @@ if (!empty($_GET['id'])) {
     $user = $userModel->findUserById($_id); //Update existing user
 }
 
-//C1: Sử dụng token CSRF
-$token = bin2hex(random_bytes(32)); // Tạo token CSRF
-$_SESSION['csrf_token'] = $token; // Lưu token trong phiên của người dùng
+//C1: Sử dụng Anti-Forgery Tokens
+// if (!isset($_SESSION['csrf_token'])) {
+//     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+// }
+
+// foreach ($_COOKIE as $name => $value) {
+//     setcookie($name, $value, [
+//         "expires" => time() + 3600, // Thời gian hết hạn của cookie
+//         "path" => "/", // Phạm vi của cookie
+//         "domain" => "", // Tên miền của cookie
+//         "secure" => true, // Gửi cookie qua kết nối an toàn (HTTPS)
+//         "samesite" => "Lax", // Giá trị SameSite (Lax hoặc Strict)
+//     ]);
+// }
+
+//c3:  Referer Header
+// if (!isset($_SERVER['HTTP_REFERER']) || parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) !== 'localhost') {
+//     header('HTTP/1.1 403 Forbidden');
+//     die('Invalid Referer');
+// }
 
 
-if (isset($_POST['csrf_token']) && $_POST['csrf_token'] === $_SESSION['csrf_token']) {
-    if (!empty($_POST['submit'])) {
+//if (isset($_POST['csrf_token']) && $_POST['csrf_token'] === $_SESSION['csrf_token']) { //c1: sử dụng Anti-Forgery Tokens
+if (!empty($_POST['submit'])) {
 
-        if (!empty($_id)) {
-            $userModel->updateUser($_POST);
-        } else {
-            $userModel->insertUser($_POST);
-        }
-        header('location: list_users.php');
+    if (!empty($_id)) {
+        $userModel->updateUser($_POST);
+    } else {
+        $userModel->insertUser($_POST);
     }
-} else {
-    header('location: messege.php');
+    header('location: list_users.php');
 }
+// } else {
+//     header('location: messege.php');
+// }
 
 
 ?>
@@ -46,28 +63,28 @@ if (isset($_POST['csrf_token']) && $_POST['csrf_token'] === $_SESSION['csrf_toke
     <div class="container">
 
         <?php if ($user || !isset($_id)) { ?>
-            <div class="alert alert-warning" role="alert">
-                User form
-            </div>
-            <form method="POST">
-                <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
-                <input type="hidden" name="id" value="<?php echo $_id ?>">
-                <div class="form-group">
-                    <label for="name">Name</label>
-                    <input class="form-control" name="name" placeholder="Name" value='<?php if (!empty($user[0]['name']))
+        <div class="alert alert-warning" role="alert">
+            User form
+        </div>
+        <form method="POST">
+            <!-- <input type="hidden" name="csrf_token" value="<?php //echo $_SESSION['csrf_token']; ?>"> -->
+            <input type="hidden" name="id" value="<?php echo $_id ?>">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input class="form-control" name="name" placeholder="Name" value='<?php if (!empty($user[0]['name']))
                         echo $user[0]['name'] ?>'>
-                    </div>
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <input type="password" name="password" class="form-control" placeholder="Password">
-                    </div>
-
-                    <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
-                </form>
-        <?php } else { ?>
-            <div class="alert alert-success" role="alert">
-                User not found!
             </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control" placeholder="Password">
+            </div>
+
+            <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <?php } else { ?>
+        <div class="alert alert-success" role="alert">
+            User not found!
+        </div>
         <?php } ?>
     </div>
 </body>
